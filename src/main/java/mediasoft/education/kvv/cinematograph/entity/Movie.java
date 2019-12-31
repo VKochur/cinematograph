e16fd3d0ad0ed394c32b65305fd58a51a8aeefcd
VKochur <v.kochurov@rambler.ru>
2019-12-31 21:32:19 +0300

    added sample servlet and entity

---
+++ src/main/java/mediasoft/education/kvv/cinematograph/entity/Movie.java
@@ -0,0 +1,36 @@
+package mediasoft.education.kvv.cinematograph.entity;
+
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "movies")
+public class Movie implements Serializable {
+
+    @Id
+    @GeneratedValue()
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/mediasoft/education/kvv/cinematograph/servlet/HelloServlet.java
@@ -0,0 +1,19 @@
+package mediasoft.education.kvv.cinematograph.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletOutputStream outputStream = resp.getOutputStream();
+        outputStream.println("hello from app");
+    }
+}
