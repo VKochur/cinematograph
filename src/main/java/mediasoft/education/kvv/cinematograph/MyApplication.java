08a6d94f48f5be8d5ee96151f23359b0667a744f
VKochur <v.kochurov@rambler.ru>
2020-01-15 09:24:08 +0300

    added swagger configuration

--- src/main/java/mediasoft/education/kvv/cinematograph/MyApplication.java
+++ src/main/java/mediasoft/education/kvv/cinematograph/MyApplication.java
@@ -1,8 +1,20 @@
 package mediasoft.education.kvv.cinematograph;
 
+import io.swagger.jaxrs.config.BeanConfig;
+
+import javax.servlet.ServletConfig;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
 
-@ApplicationPath("/")
+@ApplicationPath("/app")
 public class MyApplication extends Application {
+    public MyApplication(@Context ServletConfig servletConfig){
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1.0.0");
+        beanConfig.setTitle("Todo API");
+        beanConfig.setBasePath("/cinematograph/app");
+        beanConfig.setResourcePackage("mediasoft.education.kvv.cinematograph");
+        beanConfig.setScan(true);
+    }
 }
